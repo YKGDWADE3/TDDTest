b262041cfb9b1d7f3c2450957cbf9e8e51e20982
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-16 01:53:48 +0800

    Modify input format and judge isVaild

--- src/main/java/com/thoughtworks/MainProgram.java
+++ src/main/java/com/thoughtworks/MainProgram.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         while (true) {
-            System.out.println("输入学生个数： ");
+            Scanner input = new Scanner(System.in);
+            System.out.println("输入学生个数： (输入0则退出)");
             try {
                 int count = input.nextInt();
-                FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
-                fizzBuzzGame.start(count);
+                if (count == 0) {
+                    System.exit(0);
+                } else {
+                    FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+                    fizzBuzzGame.start(count);
+                }
             } catch (InputMismatchException i) {
-                System.out.println("输入不合法，请重新输入");
+                System.out.println("输入错误！");
             }
 
         }
